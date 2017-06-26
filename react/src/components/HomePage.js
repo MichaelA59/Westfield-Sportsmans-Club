bd93824882cab9ea2b10f63d5b7cc33e09f4a52e
Michael Adamski <mvadamski@gmail.com>
2017-06-26 19:55:35 -0400

    added nested component links on range info page

--- react/src/components/HomePage.js
+++ react/src/components/HomePage.js
@@ -6,8 +6,16 @@ import { Router, Route, Link, browserHistory } from 'react-router';
 const HomePage = (props) => {
     return(
       <div>
+        <h1 className='text-center'>Westfield Sportsmans Club</h1>
+        <h5 className='text-center'>
+          The Westfield Sportsmens Club is a member driven organization that
+          provides a safe place for its members to enjoy the shooting sports,
+          archery, fishing, and good company. We are located on over 375 acres
+          of beautiful land that not only includes the ranges, but a wooded
+          archery course, and stocked pond.
+        </h5>
+        <img src="http://www.westfieldsportsmansclub.com/images/20151128_092326.jpg" />
         <button onClick={browserHistory.goBack}>Go Back</button>
-        <h1>HomePage Page</h1>
       </div>
     )
   }
--- react/src/components/RangeInfo.js
+++ react/src/components/RangeInfo.js
@@ -1,15 +1,67 @@
 import 'babel-polyfill';
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, Link, browserHistory } from 'react-router';
 
-const RangeInfo = (props) => {
+class RangeInfo extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      whichRange: 'default'
+    }
+    this.handleHighPowerChange = this.handleHighPowerChange.bind(this);
+    this.handleMuzzelChange = this.handleMuzzelChange.bind(this);
+    this.handleOutdoorChange = this.handleOutdoorChange.bind(this);
+    this.handleTrapChange = this.handleTrapChange.bind(this);
+    this.handleSkeetChange = this.handleSkeetChange.bind(this);
+    this.handleIndoorOutdoorChange = this.handleIndoorOutdoorChange.bind(this);
+    this.handlePatternBoardsChange = this.handlePatternBoardsChange.bind(this);
+  }
+
+  handleHighPowerChange() {
+    this.setState({ whichRange: 'highpower'})
+  }
+
+  handleMuzzelChange() {
+    this.setState({whichRange: 'muzzleloader'})
+  }
+
+  handleOutdoorChange() {
+    this.setState({whichRange: 'handgunrimfire'})
+  }
+
+  handleTrapChange() {
+    this.setState({whichRange: 'trap'})
+  }
+
+  handleSkeetChange() {
+    this.setState({whichRange: 'skeet'})
+  }
+
+  handleIndoorOutdoorChange() {
+    this.setState({whichRange: 'indooroutdoor'})
+  }
+
+  handlePatternBoardsChange() {
+    this.setState({whichRange: 'patternboards'})
+  }
+
+  render () {
     return(
-      <div>
+      <div className='different-ranges'>
+        <ul>
+          <li onClick={this.handleHighPowerChange}>High Power</li>
+          <li onClick={this.handleMuzzelChange}>Muzzel Loader and Shotgun</li>
+          <li onClick={this.handleOutdoorChange}>Outdoor Handgun and Rimfire Rifles</li>
+          <li onClick={this.handleTrapChange}>Trap</li>
+          <li onClick={this.handleSkeetChange}>Skeet</li>
+          <li onClick={this.handleIndoorOutdoorChange}>Indoor/Outdoor</li>
+          <li onClick={this.handlePatternBoardsChange}>PatternBoards</li>
+        </ul>
         <button onClick={browserHistory.goBack}>Go Back</button>
-        <h1>RangeInfo Page</h1>
       </div>
     )
   }
+}
 
 export default RangeInfo;
