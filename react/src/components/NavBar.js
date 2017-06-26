758076114cdd573b108d59cd42e907a564862d7e
Michael Adamski <mvadamski@gmail.com>
2017-06-26 18:53:30 -0400

    navbar adjustments inline block

--- react/src/components/NavBar.js
+++ react/src/components/NavBar.js
@@ -6,77 +6,27 @@ import { Router, Route, Link, browserHistory } from 'react-router';
 const NavBar = (props) => {
     return (
       <div>
-        <div className='navbar navbar-default'>
-          <div className='container'>
-            <div className='navbar-header pull-left'>
+        <div className='top-bar'>
+          <div className='top-bar-left'>
+            <ul className='logo'>
+              <li className='logo-text'><Link to='/'>Westfield Sportsmans Club</Link></li>
+            </ul>
+          </div>
 
-              <ul className='nav navbar-nav'>
-                <li className='active'>
-                  <Link to='/home'>Home</Link>
-                </li>
-              </ul>
-
-              <div className='navbar-header'>
-                <ul className='nav navbar-nav'>
-                  <li>
-                    <Link to='/range'>Range Info</Link>
-                  </li>
-                </ul>
-              </div>
-
-              <div className='navbar-header'>
-                <ul className='nav navbar-nav'>
-                  <li>
-                    <Link to='/facilities'>Facilities</Link>
-                  </li>
-                </ul>
-              </div>
-
-              <div className='navbar-header'>
-                <ul className='nav navbar-nav'>
-                  <li>
-                    <Link to='/archery'>Archery</Link>
-                  </li>
-                </ul>
-              </div>
-
-              <div className='navbar-header'>
-                <ul className='nav navbar-nav'>
-                  <li>
-                    <Link to='/events'>Events</Link>
-                  </li>
-                </ul>
-              </div>
-
-              <div className='navbar-header'>
-                <ul className='nav navbar-nav'>
-                  <li>
-                    <Link to='/about'>About</Link>
-                  </li>
-                </ul>
-              </div>
-
-              <div className='navbar-header'>
-                <ul className='nav navbar-nav'>
-                  <li>
-                    <Link to='/membership'>Membership</Link>
-                  </li>
-                </ul>
-              </div>
-
-              <div className='navbar-header'>
-                <ul className='nav navbar-nav'>
-                  <li>
-                    <Link to='/fid'>Fid</Link>
-                  </li>
-                </ul>
-              </div>
-              
-            </div>
+          <div className='top-bar-right'>
+            <ul className='nav navbar-nav'>
+              <li><Link to='/range'>Range Information</Link></li>
+              <li><Link to='/facilities'>Facilities</Link></li>
+              <li><Link to='/archery'>Archery</Link></li>
+              <li><Link to='/events'>Events</Link></li>
+              <li><Link to='/about'>About</Link></li>
+              <li><Link to='/membership'>Membership</Link></li>
+              <li><Link to='/fid'>Fid</Link></li>
+            </ul>
           </div>
         </div>
       </div>
-      )
-    }
+    )
+  }
 
 export default NavBar;
