7cd6a9a2d34c3ee3f64a0c8c644f9fd8840c5036
Michael Adamski <mvadamski@gmail.com>
2017-06-28 16:23:43 -0400

    events page complete

--- react/src/components/Events.js
+++ react/src/components/Events.js
@@ -4,12 +4,28 @@ import ReactDOM from 'react-dom';
 import { Router, Route, Link, browserHistory } from 'react-router';
 
 const Events = (props) => {
-    return(
-      <div>
-        <button onClick={browserHistory.goBack}>Go Back</button>
-        <h1>Events Page</h1>
+  return(
+  <div>
+    <div className='row'>
+      <div className='columns small-12 text-center'>
+        <h1> Events </h1>
       </div>
-    )
-  }
+    </div>
+
+    <div className='row'>
+      <div className='columns small-12 text-center'>
+        <h5> We host events throughout the year </h5>
+        <h5> Trap & Skeet | Archery | Fishing | Rimfire Competitions | Long Range High Power Shoots </h5>
+      </div>
+    </div>
+
+    <div className='row'>
+      <div className='columns small-12 text-center'>
+        <button onClick={browserHistory.goBack}>Go Back</button>
+      </div>
+    </div>
+  </div>
+  )
+}
 
 export default Events;
