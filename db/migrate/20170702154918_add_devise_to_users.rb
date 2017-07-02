252554a3c8694a0e01f22232ddc2206f4d33e962
Michael Adamski <mvadamski@gmail.com>
2017-07-02 12:57:37 -0400

    devise and oauth changes

--- db/migrate/20170702154918_add_devise_to_users.rb
+++ db/migrate/20170702154918_add_devise_to_users.rb
@@ -4,7 +4,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[5.0]
       ## Database authenticatable
       t.string :first_name
       t.string :last_name
-      t.string :username, null: false
+      t.string :username
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
